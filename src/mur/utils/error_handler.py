3e9c8aa713df63d656dddd13d0c07bca9e4e0976
Emanuel <e.w.degraaff@gmail.com>
2025-01-15 21:16:36 +0100

    fix(cli): clean up __init__.py at uninstall

--- src/mur/utils/error_handler.py
+++ src/mur/utils/error_handler.py
@@ -91,6 +91,7 @@ class MurError(Exception):
         306: 'Local Artifact Metadata Invalid',
         307: 'Artifact Build Failed',
         308: 'Missing Dependencies',
+        309: 'Failed to Uninstall Artifact',
         # Category 5: Authentication/Authorization
         500: 'General Auth Error',
         501: 'Authentication Error',
